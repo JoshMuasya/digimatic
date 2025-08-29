1600876d1ce16c9548ca6a4103e8678fccb2b29d
JoshMuasya <muasyajoshua07@gmail.com>
2025-08-29 09:19:35 +0300

    Made some changes

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -138,7 +138,7 @@ const ServicesCards = () => {
 
   const cardVariants: Variants = {
     hidden: { opacity: 0, y: 30 },
-    show: { opacity: 1, y: 0, transition: { duration: 0.55, ease: "easeOut" } },
+    show: { opacity: 1, y: 0, transition: { duration: 0.3, ease: "easeOut" } },
   };
 
   return (
